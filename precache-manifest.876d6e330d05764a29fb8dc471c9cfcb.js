be85add98681ea7e5b3e394d327a2fa751b2235f
maskeynihal <maskeynihal@gmail.com>
2020-08-03 10:17:13 +0545

    Updates

---
+++ precache-manifest.876d6e330d05764a29fb8dc471c9cfcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a9db9d1d5f042f28aa51a1eea250ad",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "d2ea0a121c512cb70f50",
+    "url": "/hacker-news/static/css/main.0d0fd8c2.chunk.css"
+  },
+  {
+    "revision": "43b71327a43f21b5d417",
+    "url": "/hacker-news/static/js/2.099610c3.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/hacker-news/static/js/2.099610c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ea0a121c512cb70f50",
+    "url": "/hacker-news/static/js/main.854e5533.chunk.js"
+  },
+  {
+    "revision": "f934003405a453338133",
+    "url": "/hacker-news/static/js/runtime-main.bdc9e62f.js"
+  }
+]);
