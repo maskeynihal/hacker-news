b2c9a18fdc6871db50f7f4ccd5f610af67af7aad
maskeynihal <maskeynihal@gmail.com>
2020-08-05 09:33:02 +0545

    Updates

---
+++ precache-manifest.12f442ee2c1163ba2d8e3cdf3de30eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b9614244374068c9545a065e45a79f",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "41c97b9c45294c9bdc0f",
+    "url": "/hacker-news/static/css/main.7027dd35.chunk.css"
+  },
+  {
+    "revision": "3884c64812bc8eb2265e",
+    "url": "/hacker-news/static/js/2.564edca4.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/hacker-news/static/js/2.564edca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c97b9c45294c9bdc0f",
+    "url": "/hacker-news/static/js/main.98c8ce38.chunk.js"
+  },
+  {
+    "revision": "f934003405a453338133",
+    "url": "/hacker-news/static/js/runtime-main.bdc9e62f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hacker-news/precache-manifest.f132e19ed913fb6e5daf5aab92dfea95.js"
+  "/hacker-news/precache-manifest.12f442ee2c1163ba2d8e3cdf3de30eb2.js"
 );
 
 self.addEventListener('message', (event) => {
